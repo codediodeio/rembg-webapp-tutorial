b6c800add2c7c1cc71cb7b792d7513e9c0e6f053
101br03k <warmerdamm03@gmail.com>
2024-03-17 15:48:18 +0000

    added pr 3 on codediodeio/rembg-webapp-tutorial

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker-compose down
-docker image rm ghcr.io/101br03k/rembg-webapp/v1.0-pv-3.12.2-slim
-docker build . -t ghcr.io/101br03k/rembg-webapp/v1.0-pv-3.12.2-slim
+docker image rm ghcr.io/101br03k/rembg-webapp/v1.1-pv-3.12.2-slim
+docker build . -t ghcr.io/101br03k/rembg-webapp/v1.1-pv-3.12.2-slim
 docker-compose up -d
